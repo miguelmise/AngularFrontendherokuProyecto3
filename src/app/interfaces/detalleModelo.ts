b53249ab07d54ffd1dd77f8c91eb7b80c855e26d
Miguel Mise <mimise@espol.edu.ec>
2022-08-21 13:06:49 -0500

    frontend6

--- src/app/interfaces/detalleModelo.ts
+++
@@ -1,7 +0,0 @@
-export interface detalleModelo{
-    competidor: string,
-    fabricante: string,
-    fecha: Date,
-    id: number,
-    idModelo: number
-}
--- src/app/interfaces/modelo.ts
+++
@@ -1,6 +0,0 @@
-export interface Modelo {
-    marca: string,
-    modelo: string,
-    precio: string,
-    fecha: string
-}
